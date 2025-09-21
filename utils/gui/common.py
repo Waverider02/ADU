2d85706be0b33a999273b775b9ea8e8131d04dcf
Waverider02 <125592403+Waverider02@users.noreply.github.com>
2025-09-21 23:23:05 +0800

    1

---
+++ utils/gui/common.py
@@ -0,0 +1,50 @@
+import flet as ft
+import win32gui
+from typing import Optional, Union
+from diver import DivergentUniverse
+from abyss import Abyss
+
+class Page(ft.Page):
+    su: Optional[Union[DivergentUniverse]]
+    ab: Optional[Abyss]
+    first: int
+    bonus: bool
+
+def init_page(page: Page):
+    page.su = None
+    page.ab = None
+    page.first = 1
+
+
+def show_snack_bar(page, text, color, selectable=False):
+    return page.show_snack_bar(
+        ft.SnackBar(
+            open=True,
+            content=ft.Text(text, selectable=selectable),
+            bgcolor=color,
+        )
+    )
+
+
+def cleanup():
+    pass
+
+
+def enum_windows_callback(hwnd, hwnds, name):
+    class_name = win32gui.GetClassName(hwnd)
+    window_name = win32gui.GetWindowText(hwnd)
+    try:
+        if (
+            name == window_name
+        ):
+            hwnds.append(hwnd)
+    except:
+        pass
+    return True
+
+
+def list_handles(name='AutoDivergentUniverse'):
+    hwnds = []
+    win32gui.EnumWindows(lambda a,b:enum_windows_callback(a,b,name=name), hwnds)
+    hwnds.append(0)
+    return hwnds[0]
